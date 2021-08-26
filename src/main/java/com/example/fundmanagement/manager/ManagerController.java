cc066be3d078147acc034f5e46013394a3e4fd4a
Lu Zhao <lzhao819@usc.edu>
2021-08-26 23:31:44 +0800

    add

--- src/main/java/com/example/fundmanagement/manager/ManagerController.java
+++ src/main/java/com/example/fundmanagement/manager/ManagerController.java
@@ -54,7 +54,7 @@ public class ManagerController {
     }
 
     //某个manager名下list of (securityName, total quantity)
-    @GetMapping(path="/securityQuantList/{managerId}/")
+    @GetMapping(path="{managerId}/securityQuantList/")
     public List<String> getSecurityQuantList(@PathVariable("managerId") Integer id){
         return managerService.getSecurityQuantList(id);
     }
